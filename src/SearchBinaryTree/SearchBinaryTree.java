e8d141fd00e26448665ff258620f3916ad7bf231
Braian <braian.biale@ing.austral.edu.ar>
2019-04-18 11:32:32 -0300

    Update babe!

--- src/SearchBinaryTree/SearchBinaryTree.java
+++ src/SearchBinaryTree/SearchBinaryTree.java
@@ -30,20 +30,104 @@ public class SearchBinaryTree<T> {
 
     }
 
-    public boolean exist(Comparable x){
-        return true;
+    public boolean exist(Comparable<T> x){
+        return exist(root, x);
     }
 
     public T getMin(){
-        return null;
+        return getMin(root).elem;
     }
 
     public T getMax(){
-        return null;
+        return getMax(root).elem;
     }
 
-    public T search(Comparable x){
-        return null;
+    public T search(Comparable<T> x){
+        return search(root, x).elem;
+    }
+
+    public void insert(Comparable<T> x){
+        root = insert(root, x);
+    }
+
+    public void remove(Comparable<T> x){
+        root = remove(root, x);
+    }
+
+    private boolean exist(DoubleNode<T> t, Comparable<T> x){
+        if(t == null){
+            return false;
+        }
+        if(x.compareTo(t.elem) == 0){
+            return true;
+        } else if(x.compareTo(t.elem) < 0){
+            return exist(root.left, x);
+        } else{
+            return exist(root.right, x);
+        }
+    }
+
+    private DoubleNode<T> getMin(DoubleNode<T> t){
+        if(t.left == null){
+            return t;
+        }
+        return getMin(t.left);
+    }
+
+    private DoubleNode<T> getMax(DoubleNode<T> t){
+        if(t.right == null){
+            return t;
+        }
+        return getMax(t.right);
+    }
+
+    private DoubleNode<T> search(DoubleNode<T> t, Comparable<T> x){
+        if(x.compareTo(t.elem) == 0){
+            return t;
+        }else if(x.compareTo(t.elem) < 0){
+            return search(t.left, x);
+        } else {
+            return search(t.right, x);
+        }
+    }
+
+    private DoubleNode<T> insert(DoubleNode<T> t, Comparable<T> x){
+        if(t == null){
+            t = new DoubleNode<T>();
+            t.elem = (T) x;
+        } else if(x.compareTo(t.elem) < 0){
+            t.left = insert(t.left, x);
+        } else {
+            t.right = insert(t.right, x);
+        }
+        return t;
+    }
+
+    private DoubleNode<T> remove(DoubleNode<T> t, Comparable<T> x){
+        if(x.compareTo(t.elem) < 0){
+            t.left = remove(t.left, x);
+        } else if(x.compareTo(t.elem) > 0){
+            t.right = remove(t.right, x);
+        } else {
+            if(t.left != null && t.right != null){
+                t.elem = getMin(t.right).elem;
+                t.right = removeMin(t.right);
+            }else if(t.left != null){
+                t = t.left;
+            } else {
+                t = t.right;
+            }
+        }
+        return t;
+    }
+
+    private DoubleNode<T> removeMin(DoubleNode<T> t){
+        if(t.left != null){
+            t.left = removeMin(t.left);
+        } else{
+            t = t.right;
+        }
+        return t;
     }
 
     private class DoubleNode <T> implements Serializable {
@@ -51,8 +135,8 @@ public class SearchBinaryTree<T> {
         T elem;
         DoubleNode<T> right, left;
 
-        public DoubleNode(T o) {
-            elem = o;
+        public DoubleNode(){
+            elem = null;
         }
 
         public DoubleNode(T o, DoubleNode<T> left, DoubleNode<T> right) {
